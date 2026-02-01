4d0bac4f48c665ebefacc4d3a34ab470631477d5
Survey Puskesmas <surveypuskesmas@gmail.com>
2026-02-02 04:27:58 +0700

    chore: disable eslint during build for vercel

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
 
 export default nextConfig;
+
 
